3e519cdb9bee4d3a2b7646bb038e4fc79df42b74
Md Masbubul Karim <mdmasbubulkarim@Mds-MacBook-Pro.home>
2020-06-18 20:35:49 -0400

    Basic version
    Does not filter on main YouTube page

---
+++ Youtube Recommended Filter/content.js
@@ -0,0 +1,70 @@
+window.onload = function(){
+    var url = window.location.href;
+    console.log(url);
+    if(url.indexOf("watch?v=") != -1){
+        console.log("removing videos...")
+        filterType1();
+    }
+}
+/*will think about implememting for main page at a later time*/
+
+/*Filters the video on the next up pannel when watching a specific video*/
+function filterType1(){
+    var dict = new Map();
+    getChannelsOnScreen(dict);
+    deleteVideos(dict);
+
+    $(window).bind('mousewheel', function(event) {
+        if (event.originalEvent.wheelDelta < 0) {
+            // scrolling down
+            // update the map
+            getChannelsOnScreen(dict);
+            //delete the video elements
+            deleteVideos(dict);
+        }
+    });
+}
+
+/*returns the channel's video (element object) showing up on screen*/
+function getChannelsOnScreen(dict){
+    let related = $("#related");
+    let items = $("#related #items");
+    let recommendations = [];
+
+    recommendations = items.children();
+
+    for(elt of recommendations){
+        let name = $(elt).find("yt-formatted-string").text();
+        // add name of channel and {elt} to map if its not already in there
+        if(dict.has($.data(elt)) == false){
+            $(elt).data("theObj", elt);
+            dict.set($.data(elt), name);
+        }
+    }
+}
+
+/*returns the list of blacklisted channels*/
+function deleteVideos(dict){
+    chrome.storage.local.get('channels', function(result){
+        var channels = result.channels;
+        //get the elements to delete
+        if(channels.length != 0){
+            for(c of channels){
+                toDelete = getKeys(dict, c);
+                for(elt of toDelete){
+                    $(elt["theObj"]).remove();
+                }
+            }
+        }
+    });
+}
+
+function getKeys(map, searchValue) {
+    let res = []
+    for (let [key, value] of map) {
+        if (value == searchValue){
+            res.push(key);
+        }
+    }
+    return res;
+}
---
+++ Youtube Recommended Filter/popup.js
@@ -0,0 +1,61 @@
+window.onload = function(){
+    document.getElementById("blacklist_btn").onclick = blacklist;
+    document.getElementById("whitelist_btn").onclick = whitelist;
+    document.getElementById("clear_btn").onclick = clear;
+    document.getElementById("showAll_btn").onclick = showAll;
+
+    /*function that adds text in textbox to blacklist if blacklist_btn is
+    clicked*/
+    function blacklist(){
+        let cname = document.getElementById("cname").value;
+        chrome.storage.local.get({channels: []}, function(result){
+            var channels = result.channels;
+            let index = channels.indexOf(cname);
+            if(index == -1){
+                channels.push(cname);
+            }
+            setChannels(channels);
+        });
+        document.getElementById("cname").value = ""
+    }
+
+    /*function that removes text in textbox from blacklist if whitelist_btn is
+    clicked*/
+    function whitelist(){
+        let cname = document.getElementById("cname").value;
+        chrome.storage.local.get({channels: []}, function(result){
+            var channels = result.channels;
+            let index = channels.indexOf(cname);
+            if(index != -1){
+                channels.splice(index, 1);
+            }
+            setChannels(channels)
+        });
+        document.getElementById("cname").value = ""
+    }
+
+    /*clears the blacklist*/
+    function clear(){
+        chrome.storage.local.get({channels: []}, function(result){
+            var channels = result.channels;
+            channels.length=0;
+            setChannels(channels);
+        });
+    }
+
+    /*helper method to update the chrome storage*/
+    function setChannels(channels){
+        chrome.storage.local.set({channels: channels}, function(result){
+            chrome.storage.local.get('channels', function(result){
+                console.log(result.channels);
+            });
+        });
+    }
+
+    function showAll(){
+        chrome.storage.local.get('channels', function(result){
+            console.log(result.channels);
+            window.alert("Black Listed Channels: " + result.channels);
+        });
+    }
+}
